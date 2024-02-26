b05392bc4c6380be6d1d8bbd4bf4dfc550933270
loris3 <71146364+loris3@users.noreply.github.com>
2024-02-26 13:26:50 +0100

    adjusted logger

--- js/server/logger.mjs
+++ js/server/logger.mjs
@@ -1,8 +1,15 @@
 import winston from 'winston';
 
+
+const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      myFormat
+    ),
     transports: [
       new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
       new winston.transports.File({ filename: 'logs/app.log' }),
@@ -10,7 +17,10 @@ const logger = winston.createLogger({
   });
   if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
-      format: winston.format.simple(),
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        myFormat
+      ),
     }));
   }
 export default logger;
