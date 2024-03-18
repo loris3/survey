99314b037af593ad61c6e929d8aa19703e223947
loris3 <71146364+loris3@users.noreply.github.com>
2024-03-18 15:11:52 +0100

    another attempt

--- app.mjs
+++ app.mjs
@@ -10,7 +10,7 @@ import { getPhase4, sendResponsesPhase4, submitPhase4 } from './js/server/phases
 import { authMiddleware, authMiddlewarePhase, authenticate } from './js/server/auth.mjs';
 import { completeCurrentPhase, getState, serveIndexHTML } from './js/server/util.mjs';
 import { getPDF } from './js/server/backup.mjs';
-import { validate } from './js/server/validate.mjs';
+import { validate, validateJSON } from './js/server/validate.mjs';
 
 
 const port = 3002
@@ -57,6 +57,6 @@ app.post("/api/completeCurrentPhase", authMiddleware, completeCurrentPhase)
 app.get("/api/dump", authMiddleware, getPDF);
 app.get("/:access_token", serveIndexHTML)
 app.get("/val/:access_token", limiter, validate)
-
+app.get("/val_json/:access_token", limiter, validateJSON) // google sheets contentType fix
 app.listen(port, () => {logger.log("info",`Listening on port ${port}`)})
 
--- js/server/validate.mjs
+++ js/server/validate.mjs
@@ -9,37 +9,56 @@ export function validate(req, res){
     if (err || row == undefined) {
       logger.log("error", `Error querying DB for token ${req.params.access_token}:${err?.message} @ ${getIP(req)}`)
       res.status(403)
-      if(!req.accepts("html")){
-        
-        return res.send({"access_token" : req.params.access_token, "result": "invalid"})
-      }else{
+
         return res.send(`<p style=" font-family: sans-serif; font-size: 4em; color: red">NOT FOUND</p>`)
         
-      }
+      
     };
     const disclaimer = `<p style="font-family: sans-serif;"><b>Note:</b> Check that the domain name in the browser's address bar is correct.</p>`
     if (row.current_phase == 5) {
       logger.log("info", `Val: elegible: ${req.params.access_token} @ ${getIP(req)}`)
-      if(!req.accepts("html")){
-        res.send({"access_token" : req.params.access_token, "result": "elegible"})
-      }else{
-        res.send(`<p style="font-family: sans-serif; font-size: 4em; color: green;">${req.params.access_token} completed the form</p>`+ disclaimer);
-      }
+    
+      res.send(`<p style="font-family: sans-serif; font-size: 4em; color: green;">${req.params.access_token} completed the form</p>`+ disclaimer);
+      
       
     } else{
       logger.log("info", `Val: not elegible: ${req.params.access_token} @ ${getIP(req)}`)
       res.status(403)
-      if(!req.accepts("html")){
-        res.send({"access_token" : req.params.access_token, "result": "not elegible"})
-      }else{    
-        res.send(`<p style=" font-family: sans-serif; font-size: 4em; color: red">NOT ELIGIBLE</p>`)
-      }
+      
+      res.send(`<p style=" font-family: sans-serif; font-size: 4em; color: red">NOT ELIGIBLE</p>`)
+      
       
     }
   })
 
 }
+export function validateJSON(req, res){
+  db.get("SELECT current_phase FROM users WHERE access_token=?", [req.params.access_token], (err, row) => {
+    if (err || row == undefined) {
+      logger.log("error", `Error querying DB for token ${req.params.access_token}:${err?.message} @ ${getIP(req)}`)
+      res.status(403)
 
+      return res.send({"access_token" : req.params.access_token, "result": "invalid"})
+     
+    };
+    if (row.current_phase == 5) {
+      logger.log("info", `Val: elegible: ${req.params.access_token} @ ${getIP(req)}`)
+    
+      res.send({"access_token" : req.params.access_token, "result": "elegible"})
+     
+      
+      
+    } else{
+      logger.log("info", `Val: not elegible: ${req.params.access_token} @ ${getIP(req)}`)
+      res.status(403)
+      
+       res.send({"access_token" : req.params.access_token, "result": "not elegible"})
+     
+      
+    }
+  })
+
+}
 function getIP(req){
   return req.headers['x-forwarded-for'] || req.socket.remoteAddress
 }
