281c7d0659d8db88a0c08ae2e2b59ad3c22df9c7
loris3 <71146364+loris3@users.noreply.github.com>
2024-03-18 14:15:22 +0100

    fix for signup sheet

--- js/server/validate.mjs
+++ js/server/validate.mjs
@@ -8,16 +8,29 @@ export function validate(req, res){
   db.get("SELECT EXISTS (SELECT 1 FROM users WHERE access_token=? AND current_phase = 5)", [req.params.access_token], (err, row) => {
     if (err) {
       logger.log("error", `Error querying DB for token ${err?.message} @ ${getIP(req)}`)
-      return res.sendStatus(403)
+      if(req.accepts("json")){
+        res.send({"access_token" : req.params.access_token, "result": "not elegible"})
+      }else{
+        return res.sendStatus(403)
+      }
     };
     const disclaimer = `<p style="font-family: sans-serif;"><b>Note:</b> Check that the domain name in the browser's address bar is correct.</p>`
     if (sqliteExists(row)) {
       logger.log("info", `Val: elegible: ${req.params.access_token} @ ${getIP(req)}`)
-      res.send(`<p style="font-family: sans-serif; font-size: 4em; color: green;">${req.params.access_token} completed the form</p>`+ disclaimer);
+      if(req.accepts("json")){
+        req.send({"access_token" : req.params.access_token, "result": "completed"})
+      }else{
+        res.send(`<p style="font-family: sans-serif; font-size: 4em; color: green;">${req.params.access_token} completed the form</p>`+ disclaimer);
+      }
+      
     } else {
       logger.log("info", `Val: not elegible: ${req.params.access_token} @ ${getIP(req)}`)
+      if(req.accepts("json")){
+        res.send({"access_token" : req.params.access_token, "result": "not elegible"})
+      }else{
       res.status(403)
       res.send(`<p style=" font-family: sans-serif; font-size: 4em; color: red">NOT ELIGIBLE</p>`)
+      }
       
     }
   })
